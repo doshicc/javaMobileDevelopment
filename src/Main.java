53278ec6290ded78ba496748f9192b6cbb8b4692
doshicc <gimnasterius@gmail.com>
2026-02-09 19:49:59 +0300

    add Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        int step = 0, personX, personY, personLive = 5, sizeBoard = 5;
+        String monster = "\uD83D\uDC7B", person = "\uD83D\uDE3D";
+        String gamingField = "+ —— + —— + —— +\n"
+                + "|    |    |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "|    | " + monster + " |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "| " + person + " |    |    |\n"
+                + "+ —— + —— + —— +";
+        System.out.println("Привет! Ты готов начать игру? (ДА или НЕТ)");
+        String answer = scanner.nextLine();
+        System.out.println("Ваш ответ:\t" + answer);
+        System.out.println("Количество жизней: " + personLive);
     }
 }
+
