95211e4ba942886ebb6e210b9525a84acbc552ef
doshicc <gimnasterius@gmail.com>
2026-02-12 19:29:23 +0300

    third meeting was made

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int step = 0, personX, personY, personLive = 5, sizeBoard = 5;
+
+        int step = 0, personX = 1, personY = 3, sizeBoard = 3;
         String monster = "\uD83D\uDC7B", person = "\uD83D\uDE3D";
         String gamingField = "+ —— + —— + —— +\n"
                 + "|    |    |    |\n"
@@ -13,10 +14,43 @@ public class Main {
                 + "+ —— + —— + —— +\n"
                 + "| " + person + " |    |    |\n"
                 + "+ —— + —— + —— +";
+
         System.out.println("Привет! Ты готов начать игру? (ДА или НЕТ)");
         String answer = scanner.nextLine();
-        System.out.println("Ваш ответ:\t" + answer);
-        System.out.println("Количество жизней: " + personLive);
+
+        switch (answer){
+            case "ДА":
+                System.out.println("Выбери сложность игры(от 1 до 5):");
+                int difficultGame = scanner.nextInt();
+                System.out.println("Выбранная сложность:\t" + difficultGame);
+
+                System.out.println("Сколько жизней будет у персонажа?");
+                int personLive = scanner.nextInt();
+                System.out.println(gamingField);
+                System.out.println("Количество жизней:\t" + personLive + "\n");
+
+                System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
+                        "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+                System.out.println(x + ", " + y);
+                if (x != personX && y != personY) {
+                    System.out.println("Неккоректный ход");
+                } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                    personX = x;
+                    personY = y;
+                    step++;
+                    System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
+                            "\nХод номер: " + step);
+                } else {
+                    System.out.println("Координаты не изменены");
+                }
+            case "НЕТ":
+                System.out.println("Жаль, приходите еще.");
+                break;
+            default:
+                System.out.println("Данные введены некорректно");
+        }
     }
 }
 
